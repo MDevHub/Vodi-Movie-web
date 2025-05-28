db516158855c2accaea51524bf05486897c55b73
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-05-28 06:28:45 +0100

    Added vercel json and Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import WatchPage from './Pages/WatchPage';
 import Blog from './Pages/Blog';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
@@ -14,14 +15,15 @@ function App() {
       <NavBar />
 
       <main className="flex-grow mt-[68px]"> {/* Add top margin to offset fixed navbar */}
-        <Routes>
-          <Route path="/" element={<Login />} />        {/* Login landing page */}
-          <Route path="/home" element={<Page1 />} />    {/* Page1 as post-login home */}
-          <Route path="/watch/:id" element={<WatchPage />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+			<Routes>
+				<Route path="/" element={<Login />} />        {/* Login landing page */}
+				<Route path="/home" element={<Page1 />} />    {/* Page1 as post-login home */}
+				<Route path="/watch/:id" element={<WatchPage />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
       </main>
 
       <Footer />
